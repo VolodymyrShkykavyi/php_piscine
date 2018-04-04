cc6dfd14d250a6afc60af8b7c94a7830c48c8858
VolodymyrShkykavyi <volodymyr.shkykavyi@gmail.com>
2018-04-04 20:50:43 +0300

    repair

--- d01/ex12/search_it!.php
+++ d01/ex12/search_it!.php
@@ -6,7 +6,7 @@ $pattern = $argv[1];
 $pattern = "/" . $pattern . ":/";
 foreach (array_reverse(array_slice($argv, 2)) as $value){
     if (preg_match($pattern, $value)){
-        echo substr($value, strlen($pattern) - 2) . "\n";
+        echo substr($value, strlen($pattern) - 3) . "\n";
         break;
     }
 }
