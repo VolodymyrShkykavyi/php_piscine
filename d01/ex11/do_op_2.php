a959d9a404c6f7c2b07830545139dcde9254ee89
VolodymyrShkykavyi <volodymyr.shkykavyi@gmail.com>
2018-04-04 20:03:55 +0300

    ex11 division 0 by 0

--- d01/ex11/do_op_2.php
+++ d01/ex11/do_op_2.php
@@ -10,6 +10,10 @@ $pattern = '/^(\+|\-){0,1}[0-9]*(\+|\-|\*|\/|\%)(\+|\-){0,1}[0-9]*$/';
 if (preg_match($pattern, $argv[1])) {
     $argv[1] = preg_replace('/(\-\-)/', '- -', $argv[1]);
     $argv[1] = preg_replace('/(\+\+)/', '+ +', $argv[1]);
-    eval("echo {$argv[1]}; echo \"\n\";");
+    @eval("\$res = {$argv[1]};");
+    if (is_nan($res))
+        echo "Syntax Error\n";
+    else
+        echo $res . "\n";
 } else
     echo "Syntax Error\n";
