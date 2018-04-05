0368c4ae8b387d6c26de9949b1aa81d291c14f2b
VolodymyrShkykavyi <volodymyr.shkykavyi@gmail.com>
2018-04-05 04:53:01 +0300

    ex02

---
+++ d02/ex02/magnifying_glass.php
@@ -0,0 +1,16 @@
+#!/usr/bin/php
+<?php
+
+function str_toupper($matches){
+	return strtoupper($matches[0]);
+}
+
+function all_toupper($matches) {
+	$matches = preg_replace_callback('#(?<=>)([^<]*?)(?=<)#', 'str_toupper', $matches);
+	$matches = preg_replace_callback('#(?<=title=\")[\s\S]*?(?=\")#', 'str_toupper', $matches[0]);
+	return($matches);
+}
+
+$file = file_get_contents($argv[1]);
+$file = preg_replace_callback('#<a[ |>][\s\S]*?<\/a>#', 'all_toupper', $file);
+print_r($file);
