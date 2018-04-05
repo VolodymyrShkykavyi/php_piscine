1a0b9e132fd9aff67919e3b071bc79196bae7108
VolodymyrShkykavyi <volodymyr.shkykavyi@gmail.com>
2018-04-05 13:32:58 +0300

    main done in ex03

--- d02/ex03/who.php
+++ d02/ex03/who.php
@@ -1,4 +1,18 @@
 #!/usr/bin/php
 <?php
-$utmpx = file_get_contents("/var/run/utmpx");
-print_r($utmpx);
+$utmpx = fopen("/var/run/utmpx", 'r');
+$curr_usr = get_current_user();
+print_r($curr_usr);
+//iid->a4id
+while (($data = fread($utmpx, 628))) {
+	$tab = unpack('a256usr_name/iid/a32term_name/iprocess_id/isession_status/itimestamp/imicrosec/a256host_name/a64padding', $data);
+if ($curr_usr == trim($tab['usr_name']))
+	echo "$curr_usr\t";
+//print_r($tab);
+}
+/*
+foreach ($sub as $value) {
+$tab = unpack('a256usr_name/i4id/a32term_name/i4process_id/i4session_status/i4timestamp/i4microsec/a256host_name', $value);
+print_r($tab);
+}
+*/
