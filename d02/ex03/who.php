fbed033f55fb019894ecd9b4e07c166c41801716
VolodymyrShkykavyi <volodymyr.shkykavyi@gmail.com>
2018-04-05 15:56:19 +0300

    ex03

--- d02/ex03/who.php
+++ d02/ex03/who.php
@@ -1,18 +1,10 @@
 #!/usr/bin/php
 <?php
+date_default_timezone_set('Europe/Paris');
 $utmpx = fopen("/var/run/utmpx", 'r');
 $curr_usr = get_current_user();
-print_r($curr_usr);
-//iid->a4id
 while (($data = fread($utmpx, 628))) {
 	$tab = unpack('a256usr_name/iid/a32term_name/iprocess_id/isession_status/itimestamp/imicrosec/a256host_name/a64padding', $data);
 if ($curr_usr == trim($tab['usr_name']))
-	echo "$curr_usr\t";
-//print_r($tab);
+    printf("%s %s %s \n", $curr_usr, trim($tab['term_name']), strftime(" %b %e %H:%M", $tab['timestamp']), trim($tab['host_name']));
 }
-/*
-foreach ($sub as $value) {
-$tab = unpack('a256usr_name/i4id/a32term_name/i4process_id/i4session_status/i4timestamp/i4microsec/a256host_name', $value);
-print_r($tab);
-}
-*/
