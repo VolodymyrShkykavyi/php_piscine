5978b98e0a5d5ac8c2952a2906bbacafafb105e2
VolodymyrShkykavyi <volodymyr.shkykavyi@gmail.com>
2018-04-06 17:47:54 +0300

    ex00 done

--- d04/ex00/index.php
+++ d04/ex00/index.php
@@ -1,10 +1,8 @@
 <?php
 session_start();
-if ($_GET['submit'] == 'OK' && $_GET['login'] && $_GET['passwd']){
+if ($_GET['submit'] == 'OK'){
     $_SESSION['login'] = $_GET['login'];
     $_SESSION['passwd'] = $_GET['passwd'];
-    var_dump($_GET);
-    var_dump($_SESSION);
 }
 
 ?>
@@ -13,6 +11,6 @@ if ($_GET['submit'] == 'OK' && $_GET['login'] && $_GET['passwd']){
 Username: <input type="text" name="login" value="<?php echo $_SESSION['login'];?>" />
 <br />
 Password: <input type="password" name="passwd" value="<?php echo $_SESSION['passwd']; ?>" />
-<input type="submit" value="OK" />
+<input type="submit" value="OK" style="display: block;"/>
 </form>
 </body></html>
