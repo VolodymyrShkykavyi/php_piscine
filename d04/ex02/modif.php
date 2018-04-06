a0a5df59b46c726c60f9f48bcf37843f6c07783a
VolodymyrShkykavyi <volodymyr.shkykavyi@gmail.com>
2018-04-07 02:12:10 +0300

    ex02

--- d04/ex02/modif.php
+++ d04/ex02/modif.php
@@ -1,6 +1,23 @@
 <?php
+
 if ($_POST['submit'] != "OK" || !$_POST['login'] || !$_POST['oldpw'] || !$_POST['newpw']
     || !file_exists("../ex01/private/passwd")){
     echo "ERROR\n";
     return;
 }
+$all_users = unserialize(file_get_contents("../ex01/private/passwd"));
+foreach ($all_users as &$user){
+	if ($user['login'] === $_POST['login']){
+		if ($user['passwd'] === hash('sha256', $_POST['oldpw'])){
+			$user['passwd'] = hash('sha256', $_POST['newpw']);
+			echo "OK\n";
+			$serialized = serialize($all_users);
+			file_put_contents("private/passwd", $serialized);
+			return;
+		}
+		else{
+			break;
+		}
+	}
+}
+echo "ERROR\n";
