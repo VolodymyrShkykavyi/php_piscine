72027d43429f1648aa3a8466bb69b897b18956ae
VolodymyrShkykavyi <volodymyr.shkykavyi@gmail.com>
2018-04-10 19:30:29 +0300

    nothing

---
+++ d06/ex00/Color.class.php
@@ -0,0 +1,19 @@
+<?php
+
+class Color
+{
+    public $red;
+    public $green;
+    public $blue;
+    public static $verbose = false;
+
+    function __construct($arr)
+    {
+
+    }
+
+    function __toString()
+    {
+        // TODO: Implement __toString() method.
+    }
+}
