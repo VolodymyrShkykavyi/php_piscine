756a8ae66e58ffec8307de27d83f87dd7081feee
VolodymyrShkykavyi <volodymyr.shkykavyi@gmail.com>
2018-04-14 18:32:31 +0300

    trash

--- d09/ex02/todo.js
+++ d09/ex02/todo.js
@@ -1,20 +1,23 @@
+
 function add_todo() {
     var res = prompt("Add new TO DO");
+    var list = document.getElementById('ft_list');
     if (res) {
-        var list = document.getElementById('ft_list');
         var new_el = document.createElement('div');
         var text = document.createTextNode(res);
         new_el.appendChild(text);
-        new_el.setAttribute('onclick', "del_todo(this);");
+        new_el.setAttribute('onclick', 'del_todo(this);');
         list.insertBefore(new_el, list.firstChild);
-        document.cookie = "list=1";
     }
-    console.log(document.cookie);
+    setCookie('todo_list', encodeURI(list.innerHTML));
+    console.log(document.getElementById('ft_list').innerHTML);
 }
 
 function del_todo(elem) {
     if (confirm("delete \"" + elem.innerHTML + "\"")){
         elem.remove();
+        var list = document.getElementById('ft_list');
+        setCookie('todo_list', encodeURI(list.innerHTML));
     }
 }
 
@@ -34,7 +37,20 @@ function getCookie(cname) {
     return "";
 }
 
+
+
+function setCookie(name, value)
+{
+    var today = new Date();
+    var expiry = new Date(today.getTime() + 30 * 24 * 3600 * 1000); // plus 30 days
+    document.cookie=name + "=" + value + "; path=/; expires=" + expiry.toGMTString();
+}
+
 window.onload = function () {
-    var div = getCookie('list');
-  //  document.appendChild(div);
+
+    var data = getCookie('todo_list');
+    if (data) {
+        console.log(decodeURI(data));
+        document.getElementById('ft_list').innerHTML = decodeURI(data);
+    }
 };
